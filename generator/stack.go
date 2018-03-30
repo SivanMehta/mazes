6c609e0c0d5dd552f9d3054bb7ee73be961d9476
smehta <smehta@godaddy.com>
2018-03-30 08:47:38 -0700

    [wip] add a simple array-based stack struct

---
+++ generator/stack.go
@@ -0,0 +1,44 @@
+package maze
+
+import (
+  "log"
+)
+
+type stack struct {
+ cells []Cell
+ size int
+}
+
+func (this *stack) Push(v Cell) {
+  this.cells = append(this.cells, v)
+  this.size++
+}
+
+func (this *stack) Pop() Cell {
+  // get top element
+  top := this.cells[len(this.cells) - 1]
+
+  // slice array
+  this.cells = this.cells[:len(this.cells) - 1]
+  this.size--
+
+  // return element at the top
+  return top
+}
+
+func (this *stack) Len() int {
+  return this.size
+}
+
+func (this *stack) Peek() Cell {
+  return this.cells[len(this.cells) - 1]
+}
+
+func (this *stack) Print() {
+  log.Println(this.cells[len(this.cells) - this.size:])
+}
+
+func NewStack(size int) *stack {
+    cells := make([]Cell, size)
+    return &stack{ cells: cells, size: 0 }  // enforce the default value here
+}

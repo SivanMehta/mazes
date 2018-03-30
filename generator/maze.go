e16dd19bdde0246ab00a2da6c7f61be2449ea4de
smehta <smehta@godaddy.com>
2018-03-29 20:57:30 -0700

    [wip] scaffold out file

---
+++ generator/maze.go
@@ -0,0 +1,11 @@
+package generator
+
+import (
+  // "log"
+  // "math/rand"
+  "github.com/ajstarks/svgo"
+)
+
+func drawCells(args *Args, canvas *svg.SVG) {
+  // uses a maze algorithm to generate a solveable maze
+}
